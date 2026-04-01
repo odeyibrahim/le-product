2e7b9f8a525d07e21c3a6abf2eb929ebec0c2345
odeyibrahim <odey.helpdesk@gmail.com>
2026-04-01 12:05:18 +0100

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-export const handler = async () => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Function working!', time: new Date().toISOString() })
-    };
-};
